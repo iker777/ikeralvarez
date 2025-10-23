28529a84bed0f81cd8bb849e0eee67faecbaf045
“iker777” <ikeralvarez777@gmail.com>
2025-10-23 14:30:36 +0200

    Rama dev

---
+++ src/data/servicesData.js
@@ -0,0 +1,65 @@
+export const servicesData = [
+    {
+        id: 1,
+        title: "Webs para negocios",
+        description:
+            "La presencia online parte de una web, aquí convertimos. Queremos que nuestros clientes se sienta cómodos en nuestra casa.",
+        image: "img/coding-bg.jpg",
+        alt: "Curso de HTML y CSS",
+        buttonText: "Saber más",
+        items: [
+            "Llegar al público que te aprecia",
+            "Poner en valor tu trabajo y elevar tu imagen",
+            "También arreglo webs chapuceras",
+        ],
+    },
+    {
+        id: 2,
+        title: "Tiendas online",
+        description:
+            "¿Cuántas veces crees que una persona compra online al mes?",
+        image: "img/coding-bg.jpg",
+        alt: "Curso de HTML y CSS",
+        buttonText: "Saber más",
+        items: [
+            "Vende tus artesanias y llega a tus fans",
+            "Subida masiva de productos con IA",
+            "¿Morir de éxito? Integración del transporte con tu tienda. Olvídate de hacer etiquetas a mano.",
+            "Imagen corporativa: con ayuda de mi equipo elevaremos tu producto",
+        ],
+    },
+    {
+        id: 3,
+        title: "Asesoría 1 a 1",
+        description: "Quién no agradece una mano en los momentos duros",
+        image: "img/coding-bg.jpg",
+        alt: "Curso de HTML y CSS",
+        buttonText: "Saber más",
+        items: [
+            "¿Necesitas dirección con tu negocio? Una hora conmigo puede ahorrarte muchos problemas. Mi experiencia me avala",
+        ],
+    },
+    {
+        id: 4,
+        title: "Cursos en empresas",
+        description:
+            "10 años como formador, sé llegar y transmitir. Conmigo sales amando programar.",
+        image: "img/coding-bg.jpg",
+        alt: "Curso de HTML y CSS",
+        buttonText: "Saber más",
+        items: [],
+    },
+    {
+        id: 5,
+        title: "Software a medida",
+        description: "¿Hablamos de una aplicación web que resuelva problemas?",
+        image: "img/coding-bg.jpg",
+        alt: "Curso de HTML y CSS",
+        buttonText: "Saber más",
+        items: [
+            "Startup",
+            "Aplicación de contabilidad",
+            "¿Tienes una idea loca? ¡Cuéntamela!",
+        ],
+    },
+];
