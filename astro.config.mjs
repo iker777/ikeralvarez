c5ab6b0d8154e0656367978c52454d50c2a456ba
“iker777” <ikeralvarez777@gmail.com>
2025-10-27 14:09:25 +0100

    npm run build + sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-    adapter: netlify(),
+  adapter: netlify(),
+  site: "https://ikeralvarez.com",
+  integrations: [sitemap()],
 });
